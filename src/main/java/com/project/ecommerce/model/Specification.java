314d19d75b93af2f62b32163b118d1a4e02c5feb
anargarib23 <mehrali.anar@gmail.com>
2023-01-30 16:28:25 +0400

    Model Updated: Specification. ManyToOne relationship with product added

--- src/main/java/com/project/ecommerce/model/Specification.java
+++ src/main/java/com/project/ecommerce/model/Specification.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +22,10 @@ public class Specification {
 	
 	@Column(name="content")
 	private String content;
+	
+	@ManyToOne
+	@JoinColumn(name="product_id")
+	private Product product;
 
 	public long getId() {
 		return id;
