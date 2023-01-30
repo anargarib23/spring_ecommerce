7e164d17c5ef6fe9eb175c501494c934c7dce6a3
anargarib23 <mehrali.anar@gmail.com>
2023-01-30 16:43:39 +0400

    Service Interface Created: ProductService

---
+++ src/main/java/com/project/ecommerce/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.project.ecommerce.service;
+
+import java.util.List;
+
+import com.project.ecommerce.model.Product;
+
+public interface ProductService {
+	Product getById(long id);
+	List<Product> getAll();
+	Product delete(long id);
+	Product update(long id, Product product);
+	Product save(Product product);
+ }
