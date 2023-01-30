65cad231cf849c27e03c62e444a9606b8a222879
anargarib23 <mehrali.anar@gmail.com>
2023-01-30 12:07:19 +0400

    Model created: Specification

--- src/main/java/com/project/ecommerce/controller/GeneralController.java
+++ src/main/java/com/project/ecommerce/controller/GeneralController.java
@@ -1,5 +1,5 @@
 package com.project.ecommerce.controller;
-
+//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
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ src/main/java/com/project/ecommerce/model/Specification.java
@@ -0,0 +1,55 @@
+package com.project.ecommerce.model;
+import jakarta.persistence.Column;
+//Ashig ali 2a
+//odlar yurdu universiteti
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="specification")
+public class Specification {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="content")
+	private String content;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "Specification [id=" + id + ", name=" + name + ", content=" + content + "]";
+	}
+	
+	
+	
+}
