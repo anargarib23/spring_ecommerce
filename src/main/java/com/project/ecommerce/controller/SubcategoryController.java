2d18039a7e336da9a6e1d6472fe6320cba3b54e9
anargarib23 <mehrali.anar@gmail.com>
2023-02-03 21:01:51 +0400

    Subcategory Controller created.

---
+++ src/main/java/com/project/ecommerce/controller/SubcategoryController.java
@@ -0,0 +1,61 @@
+package com.project.ecommerce.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.ecommerce.dto.SubcategoryRequestDTO;
+import com.project.ecommerce.dto.SubcategoryResponseDTO;
+import com.project.ecommerce.mapper.SubcategoryMapper;
+import com.project.ecommerce.model.Subcategory;
+import com.project.ecommerce.service.SubcategoryService;
+
+@RestController
+public class SubcategoryController {
+	@Autowired
+	private SubcategoryMapper subcategoryMapper;
+	
+	@Autowired
+	private SubcategoryService subcategoryService;
+	
+	@GetMapping("/{id}")
+	public SubcategoryResponseDTO getSubcategory(@PathVariable Long id) {
+		Subcategory subcategory = subcategoryService.getById(id);
+		return subcategoryMapper.toSubcategoryResponseDTO(subcategory);
+	}
+	
+	@GetMapping
+	public List<SubcategoryResponseDTO> getAllSubcategories(){
+		List<Subcategory> subcategories = subcategoryService.getAll();		
+		return subcategoryMapper.toSubcategoryResponseDTOlist(subcategories);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> addSubcategory(@RequestBody SubcategoryRequestDTO request) {
+		Subcategory subcategory = subcategoryMapper.toSubcategory(request);
+		subcategoryService.add(subcategory);
+		return ResponseEntity.ok().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> updateSubcategory(@RequestBody SubcategoryRequestDTO request, @PathVariable Long id){
+		Subcategory product = subcategoryMapper.toSubcategory(request);
+		subcategoryService.update(id, product);
+		return ResponseEntity.ok().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteSubcategory(@PathVariable Long id){
+		subcategoryService.delete(id);
+		return ResponseEntity.ok().build();
+	}
+}
