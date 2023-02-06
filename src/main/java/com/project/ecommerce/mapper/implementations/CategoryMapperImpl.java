2b17070d28f4d701ffe56139520bb8e0120c3fd5
anargarib23 <mehrali.anar@gmail.com>
2023-02-06 13:34:35 +0400

    Category Mapper implemented.

---
+++ src/main/java/com/project/ecommerce/mapper/implementations/CategoryMapperImpl.java
@@ -0,0 +1,40 @@
+package com.project.ecommerce.mapper.implementations;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.project.ecommerce.dto.CategoryRequestDTO;
+import com.project.ecommerce.dto.CategoryResponseDTO;
+import com.project.ecommerce.mapper.CategoryMapper;
+import com.project.ecommerce.model.Category;
+
+public class CategoryMapperImpl implements CategoryMapper {
+
+	@Override
+	public CategoryResponseDTO toCategoryResponseDTO(Category category) {
+		CategoryResponseDTO categoryResponseDTO = new CategoryResponseDTO();
+		categoryResponseDTO.setId(category.getId());
+		categoryResponseDTO.setSubcategories(category.getSubcategories());
+		return categoryResponseDTO;
+	}
+
+	@Override
+	public List<CategoryResponseDTO> toCategoryResponseDTOlist(List<Category> categories) {
+		List<CategoryResponseDTO> categoryResponseDTOlist = new LinkedList<>();
+		
+		for(Category category : categories) {
+			CategoryResponseDTO categoryResponseDTO = toCategoryResponseDTO(category);
+			categoryResponseDTOlist.add(categoryResponseDTO);
+		}
+		
+		return categoryResponseDTOlist;
+	}
+
+	@Override
+	public Category toCategory(CategoryRequestDTO categoryRequestDTO) {
+		Category category = new Category();
+		category.setSubcategories(categoryRequestDTO.getSubcategories());
+		return category;
+	}
+
+}
