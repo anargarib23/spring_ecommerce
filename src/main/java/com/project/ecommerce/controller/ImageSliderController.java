e2b721b8d6972d7b03b1c8d651aac41519406b9b
anargarib23 <mehrali.anar@gmail.com>
2023-02-06 13:28:02 +0400

    RequestMapping annotation added to controller classes. Main Controller deleted.

--- src/main/java/com/project/ecommerce/controller/ImageSliderController.java
+++ src/main/java/com/project/ecommerce/controller/ImageSliderController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.project.ecommerce.dto.ImageSliderRequestDTO;
 import com.project.ecommerce.dto.ImageSliderResponseDTO;
@@ -18,6 +20,8 @@ import com.project.ecommerce.model.ImageSlider;
 
 import com.project.ecommerce.service.ImageSliderService;
 
+@RestController
+@RequestMapping("/image-slider")
 public class ImageSliderController {
 	
 	@Autowired
--- src/main/java/com/project/ecommerce/controller/SubcategoryController.java
+++ src/main/java/com/project/ecommerce/controller/SubcategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.ecommerce.dto.SubcategoryRequestDTO;
@@ -19,6 +20,7 @@ import com.project.ecommerce.model.Subcategory;
 import com.project.ecommerce.service.SubcategoryService;
 
 @RestController
+@RequestMapping("/subcategory")
 public class SubcategoryController {
 	@Autowired
 	private SubcategoryMapper subcategoryMapper;
