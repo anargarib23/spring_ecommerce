5ef84425f601409e0ba31a47b9c171e17ec1e58a
anargarib23 <mehrali.anar@gmail.com>
2023-02-06 14:13:19 +0400

    changes

--- src/main/java/com/project/ecommerce/controller/CategoryController.java
+++ src/main/java/com/project/ecommerce/controller/CategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.ecommerce.dto.CategoryRequestDTO;
@@ -20,6 +21,7 @@ import com.project.ecommerce.service.CategoryService;
 
 
 @RestController
+@RequestMapping("/category")
 public class CategoryController {
 	@Autowired
 	private CategoryMapper categoryMapper;
