2413ee9af2b98948d0ff42e31c72025f6a56b808
anargarib23 <mehrali.anar@gmail.com>
2023-02-06 13:18:45 +0400

    ImageSlider Controller created.

--- src/main/java/com/project/ecommerce/controller/ImageSliderController.java
+++ src/main/java/com/project/ecommerce/controller/ImageSliderController.java
@@ -1,6 +1,20 @@
 package com.project.ecommerce.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.project.ecommerce.dto.ImageSliderRequestDTO;
+import com.project.ecommerce.dto.ImageSliderResponseDTO;
+
+import com.project.ecommerce.mapper.ImageSliderMapper;
+import com.project.ecommerce.model.ImageSlider;
 
 import com.project.ecommerce.service.ImageSliderService;
 
@@ -11,5 +25,33 @@ public class ImageSliderController {
 	
 	@Autowired
 	private ImageSliderService imageSliderService;
+	
+	@GetMapping("/{id}")
+	public ImageSliderResponseDTO getImageSlider(@PathVariable Long id) {
+		ImageSlider imageSlider = imageSliderService.getById(id);
+		return imageSliderMapper.toImageSliderResponseDTO(imageSlider);
+	}
+	
+	@GetMapping
+	public List<ImageSliderResponseDTO> getAllImageSliders(){
+		List<ImageSlider> imageSliders = imageSliderService.getAll();		
+		return imageSliderMapper.toImageSliderResponseDTOlist(imageSliders);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> addImageSlider(@RequestBody ImageSliderRequestDTO request) {
+		ImageSlider imageSlider = imageSliderMapper.toImageSlider(request);
+		imageSliderService.add(imageSlider);
+		return ResponseEntity.ok().build();
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteImageSlider(@PathVariable Long id){
+		imageSliderService.delete(id);
+		return ResponseEntity.ok().build();
+	}
+	
 
 }
